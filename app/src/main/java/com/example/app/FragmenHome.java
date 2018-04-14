e3ebf28c15ec45b5008d600e503b317a8c4d02a8
Ws <346623325@qq.com>
2018-04-14 21:36:10 +0800

    Ws0414

--- app/src/main/java/com/example/app/FragmenHome.java
+++ app/src/main/java/com/example/app/FragmenHome.java
@@ -1,20 +1,72 @@
 package com.example.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 
 
+import android.widget.Button;
 
-public class FragmenHome extends Fragment
-{
+import com.example.app.QingBao.zhuangxiuzhongActivity;
+
+
+public class FragmenHome extends Fragment {
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
+                             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onActivityCreated(savedInstanceState);
+        Button btn = (Button) getActivity().findViewById(R.id.button_zhuangxiuqian);
+        Button btn2= (Button) getActivity().findViewById(R.id.button_zhuangxiuhou);
+        Button btn3= (Button) getActivity().findViewById(R.id.button_qingbao);
+        Button btn4= (Button) getActivity().findViewById(R.id.button_zhuangxiucailiao);
+        Button btn5= (Button) getActivity().findViewById(R.id.button_worker);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), zhuangxiuqianActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), zhuangxiuzhongActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //button点击事件
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //button点击事件
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //button点击事件
+            }
+        });
+
     }
 }
 
