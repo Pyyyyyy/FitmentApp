b0e9416244f978e0c44724ed0b4e02cef4ce50f9
Ws <346623325@qq.com>
2018-04-15 16:38:35 +0800

    WS0415

--- app/src/main/java/com/example/app/FragmenMy.java
+++ app/src/main/java/com/example/app/FragmenMy.java
@@ -1,10 +1,13 @@
 package com.example.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import com.example.app.WoDe.LoginActivity;
 
 public class FragmenMy extends Fragment
 {
@@ -13,5 +16,21 @@ public class FragmenMy extends Fragment
     {
         View view = inflater.inflate(R.layout.user_layout, container, false);
         return view;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onActivityCreated(savedInstanceState);
+        Button btn = (Button) getActivity().findViewById(R.id.enter);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
